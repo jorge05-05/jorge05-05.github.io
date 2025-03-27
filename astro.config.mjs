969c8a84e50cf7762cea497971a6327f22668829
Jorge Castilla <47054857@continental.edu.pe>
2025-03-27 15:52:21 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://jorge05-05.github.io',
     integrations: [
         mdx(),
         sitemap(),
